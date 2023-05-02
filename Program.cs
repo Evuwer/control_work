6d31c1ea2d222934daa3d753f7ad7a3644ff19c3
Alex <evuwer@gmail.com>
2023-05-02 19:00:27 +0300

    код

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// задаем исходный массив строк
+string[] inputArray = { "яблоко", "кот", "слон", "пес", "попугай" };
+
+// создаем новый массив строк, содержащий только строки длины <= 3
+string[] outputArray = new string[inputArray.Length];
+int outputIndex = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+	if (inputArray[i].Length <= 3)
+	{
+		outputArray[outputIndex] = inputArray[i];
+		outputIndex++;
+	}
+}
+
+// выводим полученный массив на экран
+Console.WriteLine("Массив строк длины <= 3:");
+for (int i = 0; i < outputIndex; i++)
+{
+	Console.WriteLine(outputArray[i]);
+}
